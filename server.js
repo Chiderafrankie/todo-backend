8c462e00d0ee8ee7b86ae26b1a4ae3ec12635630
Chidera Frankie <frankiechideraprincess@gmail.com>
2026-06-15 12:32:58 +0100

    Add environment variables and dotenv config

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 app.use(express.json())
 const todoRoutes = require('./routes/todo.routes')
 app.use(todoRoutes)
 
-app.listen(4040, () => {
-    console.log('Server is running on port 4040')
+const PORT = process.env.PORT || 4040
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
